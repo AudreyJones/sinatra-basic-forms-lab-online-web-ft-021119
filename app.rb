e201b757450e0aaf2f9d83cb47567758af9513a8
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-04 18:11:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @name = puppy.name
     @breed = puppy.breed
     binding.pry
-    puppy.age = params["age"]
+    puppy.months_old = params["age"]
     erb :display_puppy
   end
 end
